199f611c983ba6ccbe5bdc09c0b5c04f9927d6ba
xXTitoTony <S0yt1t0t0n1@gmail.com>
2025-05-27 23:15:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
